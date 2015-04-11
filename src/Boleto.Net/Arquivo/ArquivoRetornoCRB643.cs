79ce50226d729e062637faf3bffc307329c464bc
Alberto Monteiro <alberto.monteiro@live.com>
2015-04-11 16:54:00 -0300

    Inicio da implementação do arquivo de retorno CBR643

---
+++ src/Boleto.Net/Arquivo/ArquivoRetornoCRB643.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace BoletoNet.Arquivo
+{
+    public class ArquivoRetornoCRB643 : AbstractArquivoRetorno, IArquivoRetorno
+    {
+        public override void LerArquivoRetorno(IBanco banco, Stream arquivo)
+        {
+            base.LerArquivoRetorno(banco, arquivo);
+        }
+    }
+}
