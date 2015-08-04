a00403a41618b0a6c310d4d937fed9a7b504e0e2
Ederson Nascimento <ederson.nascimento@transdatasmart.com.br>
2015-08-04 17:28:07 -0300

    MAX201506-77 #comment Implementação do depósito identificado

---
+++ src/Boleto.Net/DepositoIdentificado/DepositoIdentificado.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BoletoNet
+{
+    public abstract class DepositoIdentificado
+    {
+        public IBanco Banco { get; private set; }
+        public Cedente Cedente { get; set; }
+        public Sacado Sacado { get; set; }
+        public string Logo { get; set; }
+        public abstract string NumeroDocumento { get; set; }
+        public List<String> InstrucoesPagamento { get; set; }
+        public List<String> InstrucoesCaixa { get; set; }
+
+        public DepositoIdentificado(IBanco banco)
+        {
+            this.Banco = banco;
+        }
+
+
+    }
+}
