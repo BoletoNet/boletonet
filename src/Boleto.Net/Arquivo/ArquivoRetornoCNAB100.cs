e70c1416615c6d160803e77ff9915706a5fd691f
Ederson Nascimento <ederson.nascimento@transdatasmart.com.br>
2015-08-10 07:53:18 -0300

    MAX201506-76 #comment Implementação da interpretação do arquivo de retorno CNAB100

---
+++ src/Boleto.Net/Arquivo/ArquivoRetornoCNAB100.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace BoletoNet
+{
+    public class ArquivoRetornoCNAB100 : AbstractArquivoRetorno, IArquivoRetorno
+    {
+        public List<AbstractDetalheSegmento> ListaDetalhe { get; set; }
+
+        public override void LerArquivoRetorno(IBanco banco, Stream arquivo)
+        {
+            this.Banco = banco;
+            try
+            {
+                StreamReader stream = new StreamReader(arquivo, System.Text.Encoding.UTF8);
+                string linha = stream.ReadLine();
+
+                // Próxima linha (DETALHE)
+                while ((linha = stream.ReadLine()) != null)
+                {
+                    AbstractDetalheSegmento detalhe = DetalheRetornoCNAB100Factory.Create(linha);
+                    ListaDetalhe.Add(detalhe);
+                    OnLinhaLida(detalhe, linha);
+                }
+
+                stream.Close();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Erro ao ler arquivo.", ex);
+            }
+        }
+    }
+}
