9c78025ebc7ad2be52aacd30b29f2296a0f4b149
carloscds <cds@cds-software.com.br>
2015-12-16 15:51:56 -0200

    Adicionado NReco.PdfGenerator.dll ao pacote Nuget e ajustado para Visual Studio 2015

--- src/Boleto.Net/Util/Extensions.cs
+++ src/Boleto.Net/Util/Extensions.cs
@@ -2,9 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.Threading;
 
 namespace BoletoNet.Util
