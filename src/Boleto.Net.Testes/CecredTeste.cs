0356bf00a0a1591b9bccce5512ac4ae58a59df3a
francisco-nicolaujr <francisco.nicolaujr@gmail.com>
2016-02-22 15:38:33 -0300

    - Removido referencia a System.Threading.Tasks.

--- src/Boleto.Net.Testes/CecredTeste.cs
+++ src/Boleto.Net.Testes/CecredTeste.cs
@@ -11,7 +11,7 @@ namespace Boleto.Net.Testes {
             DateTime vencimento = new DateTime(2016, 03, 11);
 
             var cedente = new Cedente("00.000.000/0000-00", "Empresa Teste", "0109", "265071", "1");
-            cedente.Convenio = 0110041; 
+            cedente.Convenio = 0110041;
             cedente.Endereco = new Endereco() {
                 Bairro = "BAIRRO CEDENTE",
                 CEP = "88309-600",
@@ -42,28 +42,24 @@ namespace Boleto.Net.Testes {
             };
 
             BoletoNet.Boleto boleto = new BoletoNet.Boleto(vencimento, 10.99m, cedente.Carteira, nossoNumero, cedente);
+            boleto.NumeroDocumento = "ABC123";
             boleto.Sacado = sacado;
             BoletoNet.BoletoBancario boletoBancario = new BoletoBancario();
             boletoBancario.CodigoBanco = 85;    // CECRED
             boletoBancario.Boleto = boleto;
 
-            try {
-                boletoBancario.Boleto.Valida();
-                var htmlString = boletoBancario.MontaHtml();
+            boletoBancario.Boleto.Valida();
+            var htmlString = boletoBancario.MontaHtml();
 
-                //string _arquivo = string.Empty;
-                //_arquivo = string.Format(@"C:\temp\boleto_{0}.html", boletoBancario.Boleto.NumeroDocumento);
+            //string _arquivo = string.Empty;
+            //_arquivo = string.Format(@"C:\temp\boleto_{0}.html", boletoBancario.Boleto.NumeroDocumento);
 
-                //using (System.IO.FileStream f = new System.IO.FileStream(_arquivo, System.IO.FileMode.Create)) {
-                //    System.IO.StreamWriter w = new System.IO.StreamWriter(f, System.Text.Encoding.UTF8);
-                //    w.Write(htmlString);
-                //}
+            //using (System.IO.FileStream f = new System.IO.FileStream(_arquivo, System.IO.FileMode.Create)) {
+            //    System.IO.StreamWriter w = new System.IO.StreamWriter(f, System.Text.Encoding.UTF8);
+            //    w.Write(htmlString);
+            //}
 
-                Assert.IsFalse(string.IsNullOrEmpty(htmlString));
-            } catch (Exception ex) {
-                Console.WriteLine(ex.Message);
-                Assert.IsTrue(false);
-            }
+            Assert.IsTrue(!string.IsNullOrEmpty(htmlString));
 
         }
     }
--- src/Boleto.Net/EDI/Banco/TRegistroEDI_Cecred_Retorno.cs
+++ src/Boleto.Net/EDI/Banco/TRegistroEDI_Cecred_Retorno.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace BoletoNet.EDI.Banco {
     class TRegistroEDI_Cecred_Retorno : TRegistroEDI {
