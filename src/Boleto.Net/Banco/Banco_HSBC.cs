47d0e5fe10e36b65e0340d6b3c3880032d8c6ad3
LEandroCDS <leandro@cds-software.com.br>
2016-05-04 16:30:47 -0300

    Acertos remessa: Bancos do Brasil, HSBC, Itau e Sicredi

--- src/Boleto.Net/Banco/Banco_HSBC.cs
+++ src/Boleto.Net/Banco/Banco_HSBC.cs
@@ -149,11 +149,10 @@ namespace BoletoNet
                     case "CSB": boleto.CodigoBarra.Codigo =
                         // Código de Barras
                         //banco & moeda & fator & valor & nossonumero & dac_nossonumero & agencia & conta & digitosconta & "00" & "1"
-                        string.Format("{0}{1}{2}{3}{4}{5}{6}{7}001", Codigo, boleto.Moeda,
+                         string.Format("{0}{1}{2}{3}{4}{5}{6}001", Codigo, boleto.Moeda,
                                       FatorVencimento(boleto), valorBoleto, boleto.NossoNumero + _dacNossoNumero,
                                       Utils.FormatCode(boleto.Cedente.ContaBancaria.Agencia, 4),
-                                      Utils.FormatCode(boleto.Cedente.ContaBancaria.Conta, 5),
-                                      Utils.FormatCode(boleto.Cedente.ContaBancaria.DigitoConta, 2));
+                                      Utils.FormatCode(boleto.Cedente.ContaBancaria.Conta + boleto.Cedente.ContaBancaria.DigitoConta, 7));                        
                         break;
                     case "CNR":
                         // Código de Barras
