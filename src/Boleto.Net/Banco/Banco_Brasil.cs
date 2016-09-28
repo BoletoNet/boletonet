6a06fa3efc96218f83656b719c743c0b506bf239
Edson <Edson@DESKTOP-BJUSCO6>
2016-09-28 09:34:55 -0300

    Banco do Brasil - Registro R - Envio de % de Multa (boleto.PercMulta)

--- src/Boleto.Net/Banco/Banco_Brasil.cs
+++ src/Boleto.Net/Banco/Banco_Brasil.cs
@@ -1555,20 +1555,24 @@ namespace BoletoNet
                 {
                     // Código da multa 2 - percentual
                     _segmentoR += "2";
-                }
-                else if (boleto.ValorMulta > 0)
+                    _segmentoR += Utils.FitStringLength(boleto.DataMulta.ToString("ddMMyyyy"), 8, 8, '0', 0, true, true, false);
+                    _segmentoR += Utils.FitStringLength(boleto.PercMulta.ToString("0.00").Replace(",", ""), 15, 15, '0', 0, true, true, true);
+
+                } else if (boleto.ValorMulta > 0)
                 {
                     // Código da multa 1 - valor fixo
                     _segmentoR += "1";
-                }
-                else
+                    _segmentoR += Utils.FitStringLength(boleto.DataMulta.ToString("ddMMyyyy"), 8, 8, '0', 0, true, true, false);
+                    _segmentoR += Utils.FitStringLength(boleto.ValorMulta.ToString("0.00").Replace(",", ""), 15, 15, '0', 0, true, true, true);
+
+                } else
                 {
                     // Código da multa 0 - sem multa
                     _segmentoR += "0";
+                    _segmentoR += Utils.FitStringLength(boleto.DataMulta.ToString("ddMMyyyy"), 8, 8, '0', 0, true, true, false);
+                    _segmentoR += Utils.FitStringLength(boleto.ValorMulta.ToString("0.00").Replace(",", ""), 15, 15, '0', 0, true, true, true);
                 }
 
-                _segmentoR += Utils.FitStringLength(boleto.DataMulta.ToString("ddMMyyyy"), 8, 8, '0', 0, true, true, false);
-                _segmentoR += Utils.FitStringLength(boleto.ValorMulta.ToString("0.00").Replace(",", ""), 15, 15, '0', 0, true, true, true);
                 _segmentoR += _brancos110;
                 _segmentoR += "0000000000000000"; //16 zeros
                 _segmentoR += " "; //1 branco
