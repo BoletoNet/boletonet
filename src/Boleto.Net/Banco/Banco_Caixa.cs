f42df0a62d94c09cce4541a08505ef3b0cc67097
ecfenix1 <eduardo@datawin.com.br>
2016-09-28 11:15:19 -0300

    Boleto Caixa - Impressão Nosso Número Correção
    Ao gerar boleto da Caixa, não está completando o nosso número com zeros
    a esquerda. Melhoria 2770, revision 12540.

--- src/Boleto.Net/Banco/Banco_Caixa.cs
+++ src/Boleto.Net/Banco/Banco_Caixa.cs
@@ -387,11 +387,12 @@ namespace BoletoNet
             }
             else
             {
+                if(Convert.ToInt64(boleto.NossoNumero).ToString().Length < 10)
+                    boleto.NossoNumero = Utils.FormatCode(boleto.NossoNumero, 10);
+
                 if (boleto.NossoNumero.Length != 10)
-                {
                     throw new Exception(
                         "Nosso Número inválido, Para Caixa Econômica carteira indefinida, o Nosso Número deve conter 10 caracteres.");
-                }
 
                 if (!boleto.Cedente.Codigo.Equals(0))
                 {
@@ -402,9 +403,7 @@ namespace BoletoNet
                         boleto.Cedente.DigitoCedente = Convert.ToInt32(dvCodigoCedente);
                 }
                 else
-                {
                     throw new Exception("Informe o código do cedente.");
-                }
             }
 
             if (boleto.Cedente.DigitoCedente == -1)
