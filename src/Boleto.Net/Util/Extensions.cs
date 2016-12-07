9fc1cbd35b5da619a63fbb3d7f491338c8b36b8f
David Silva <davaug@gmail.com>
2016-12-07 18:57:30 -0200

    Compatibilidade com .Net v3.5

--- src/Boleto.Net/Util/Extensions.cs
+++ src/Boleto.Net/Util/Extensions.cs
@@ -93,5 +93,16 @@ namespace BoletoNet.Util
             return str.Substring(str.Length - length);
         }
 
+        public static bool IsNullOrWhiteSpace(String value)
+        {
+            if (value == null) return true;
+
+            for (int i = 0; i < value.Length; i++)
+            {
+                if (!Char.IsWhiteSpace(value[i])) return false;
+            }
+
+            return true;
+        }
     }
 }
