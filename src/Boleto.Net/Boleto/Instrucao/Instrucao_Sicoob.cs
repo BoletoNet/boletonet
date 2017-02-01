f1440e91216632d52ce9cb2e9dc90d0e0ea00b29
ecfenix1 <eduardo@datawin.com.br>
2017-02-01 16:09:28 -0200

    Boleto e Remessa Sicoob
    - Alteração do fator de vencimento, correção solicitada durante a
    homologação com o banco, revision 12857 e 12861.
    - Arquivo de retorno, Sicooob, carteira 1 - Linha de Baixa.
    - Inclusão do Cnab 400 Segmento T e U, revision 14009.
    - Remessa Sicoob 240
    * Criação do header
    * Criação do lote da remessa
    * Criação do detalhe
    * Criação do segmento R
    * Criação do trailer do lote
    * Criação do trailer da remessa
    - Na especificação do banco é solicitado sempre incluir o código do
    convênio em branco, revision 17519.
    - Inclusão do dígito da agência em homologação com o banco, revision
    17946.
    - Em homologação com o banco, foi solicitado a remoção do dígito do
    cedente para cálculo do mod11.
    - 4774 - Chamado 191133 - Críticas na homologação do boleto Sicoob,
    revision 18257.
    - Mudança da criação da linha digitável conforme solicitação da
    homologação com o banco Sicoob, revision 18432.

--- src/Boleto.Net/Boleto/Instrucao/Instrucao_Sicoob.cs
+++ src/Boleto.Net/Boleto/Instrucao/Instrucao_Sicoob.cs
@@ -1,40 +1,37 @@
 namespace BoletoNet
 {
-	using System;
+    using System;
+    #region EnumInstrucoes_Sicoob enum
+    public enum EnumInstrucoes_Sicoob
+    {
+        AusenciaDeInstrucoes = 0,
 
-	public class Instrucao_Sicoob : AbstractInstrucao, IInstrucao
+        CobrarJuros = 1,
+
+        Protestar3DiasUteis = 3,
+
+        Protestar4DiasUteis = 4,
+
+        Protestar5DiasUteis = 5,
+
+        NaoProtestar = 7,
+
+        Protestar10DiasUteis = 10,
+
+        Protestar15DiasUteis = 15,
+
+        Protestar20DiasUteis = 20,
+
+        ConcederDescontoApenasAteDataEstipulada = 22,
+
+        DevolverApos15DiasVencido = 42,
+
+        DevolverApos30DiasVencido = 43
+    }
+    #endregion
+    
+    public class Instrucao_Sicoob : AbstractInstrucao, IInstrucao
 	{
-		#region EnumInstrucoes_Sicoob enum
-
-		public enum EnumInstrucoes_Sicoob
-		{
-			AusenciaDeInstrucoes = 0, 
-
-			CobrarJuros = 1, 
-
-			Protestar3DiasUteis = 3, 
-
-			Protestar4DiasUteis = 4, 
-
-			Protestar5DiasUteis = 5, 
-
-			NaoProtestar = 7, 
-
-			Protestar10DiasUteis = 10, 
-
-			Protestar15DiasUteis = 15, 
-
-			Protestar20DiasUteis = 20, 
-
-			ConcederDescontoApenasAteDataEstipulada = 22, 
-
-			DevolverApos15DiasVencido = 42, 
-
-			DevolverApos30DiasVencido = 43
-		}
-
-		#endregion
-
 		#region Construtores
 
 		public Instrucao_Sicoob()
@@ -73,79 +70,79 @@ namespace BoletoNet
 
 		#region Metodos Privados
 
-		private void carregar(int idInstrucao, int nrDias, double percentualMultaDia)
-		{
-			try
-			{
-				this.Banco = new Banco_Banrisul();
-				this.Valida();
+        private void carregar(int idInstrucao, int nrDias, double percentualMultaDia)
+        {
+            try
+            {
+                this.Banco = new Banco_Banrisul();
+                this.Valida();
 
-				switch ((EnumInstrucoes_Sicoob)idInstrucao)
-				{
-					case EnumInstrucoes_Sicoob.AusenciaDeInstrucoes:
-						break;
-					case EnumInstrucoes_Sicoob.CobrarJuros:
-						this.Codigo = (int)EnumInstrucoes_Sicoob.CobrarJuros;
-						this.Descricao = "Cobrar Juros";
-						break;
-					case EnumInstrucoes_Sicoob.Protestar3DiasUteis:
-						this.Codigo = (int)EnumInstrucoes_Sicoob.Protestar3DiasUteis;
-						this.Descricao = "Protestar 3 dias úteis após vencimento";
-						break;
-					case EnumInstrucoes_Sicoob.Protestar4DiasUteis:
-						this.Codigo = (int)EnumInstrucoes_Sicoob.Protestar4DiasUteis;
-						this.Descricao = "Protestar 4 dias úteis após vencimento";
-						break;
-					case EnumInstrucoes_Sicoob.Protestar5DiasUteis:
-						this.Codigo = (int)EnumInstrucoes_Sicoob.Protestar5DiasUteis;
-						this.Descricao = "Protestar 5 dias úteis após vencimento";
-						break;
-					case EnumInstrucoes_Sicoob.NaoProtestar:
-						this.Codigo = (int)EnumInstrucoes_Sicoob.NaoProtestar;
-						this.Descricao = "Não protestar";
-						break;
-					case EnumInstrucoes_Sicoob.Protestar10DiasUteis:
-						this.Codigo = (int)EnumInstrucoes_Sicoob.Protestar10DiasUteis;
-						this.Descricao = "Protestar 10 dias úteis após vencimento";
-						break;
-					case EnumInstrucoes_Sicoob.Protestar15DiasUteis:
-						this.Codigo = (int)EnumInstrucoes_Sicoob.Protestar15DiasUteis;
-						this.Descricao = "Protestar 15 dias úteis após vencimento";
-						break;
-					case EnumInstrucoes_Sicoob.Protestar20DiasUteis:
-						this.Codigo = (int)EnumInstrucoes_Sicoob.Protestar20DiasUteis;
-						this.Descricao = "Protestar 20 dias úteis após vencimento";
-						break;
-					case EnumInstrucoes_Sicoob.ConcederDescontoApenasAteDataEstipulada:
-						this.Codigo = (int)EnumInstrucoes_Sicoob.ConcederDescontoApenasAteDataEstipulada;
-						this.Descricao = "Conceder desconto só até a data estipulada";
-						break;
-					case EnumInstrucoes_Sicoob.DevolverApos15DiasVencido:
-						this.Codigo = (int)EnumInstrucoes_Sicoob.DevolverApos15DiasVencido;
-						this.Descricao = "Devolver após 15 dias vencido";
-						break;
-					case EnumInstrucoes_Sicoob.DevolverApos30DiasVencido:
-						this.Codigo = (int)EnumInstrucoes_Sicoob.DevolverApos30DiasVencido;
-						this.Descricao = "Devolver após 30 dias vencido";
-						break;
-					default:
-						this.Codigo = 0;
-						this.Descricao = " (Selecione) ";
-						break;
+                switch ((EnumInstrucoes_Sicoob)idInstrucao)
+                {
+                    case EnumInstrucoes_Sicoob.AusenciaDeInstrucoes:
+                        break;
+                    case EnumInstrucoes_Sicoob.CobrarJuros:
+                        this.Codigo = (int)EnumInstrucoes_Sicoob.CobrarJuros;
+                        this.Descricao = "Cobrar Juros";
+                        break;
+                    case EnumInstrucoes_Sicoob.Protestar3DiasUteis:
+                        this.Codigo = (int)EnumInstrucoes_Sicoob.Protestar3DiasUteis;
+                        this.Descricao = "Protestar 3 dias úteis após vencimento";
+                        break;
+                    case EnumInstrucoes_Sicoob.Protestar4DiasUteis:
+                        this.Codigo = (int)EnumInstrucoes_Sicoob.Protestar4DiasUteis;
+                        this.Descricao = "Protestar 4 dias úteis após vencimento";
+                        break;
+                    case EnumInstrucoes_Sicoob.Protestar5DiasUteis:
+                        this.Codigo = (int)EnumInstrucoes_Sicoob.Protestar5DiasUteis;
+                        this.Descricao = "Protestar 5 dias úteis após vencimento";
+                        break;
+                    case EnumInstrucoes_Sicoob.NaoProtestar:
+                        this.Codigo = (int)EnumInstrucoes_Sicoob.NaoProtestar;
+                        this.Descricao = "Não protestar";
+                        break;
+                    case EnumInstrucoes_Sicoob.Protestar10DiasUteis:
+                        this.Codigo = (int)EnumInstrucoes_Sicoob.Protestar10DiasUteis;
+                        this.Descricao = "Protestar 10 dias úteis após vencimento";
+                        break;
+                    case EnumInstrucoes_Sicoob.Protestar15DiasUteis:
+                        this.Codigo = (int)EnumInstrucoes_Sicoob.Protestar15DiasUteis;
+                        this.Descricao = "Protestar 15 dias úteis após vencimento";
+                        break;
+                    case EnumInstrucoes_Sicoob.Protestar20DiasUteis:
+                        this.Codigo = (int)EnumInstrucoes_Sicoob.Protestar20DiasUteis;
+                        this.Descricao = "Protestar 20 dias úteis após vencimento";
+                        break;
+                    case EnumInstrucoes_Sicoob.ConcederDescontoApenasAteDataEstipulada:
+                        this.Codigo = (int)EnumInstrucoes_Sicoob.ConcederDescontoApenasAteDataEstipulada;
+                        this.Descricao = "Conceder desconto só até a data estipulada";
+                        break;
+                    case EnumInstrucoes_Sicoob.DevolverApos15DiasVencido:
+                        this.Codigo = (int)EnumInstrucoes_Sicoob.DevolverApos15DiasVencido;
+                        this.Descricao = "Devolver após 15 dias vencido";
+                        break;
+                    case EnumInstrucoes_Sicoob.DevolverApos30DiasVencido:
+                        this.Codigo = (int)EnumInstrucoes_Sicoob.DevolverApos30DiasVencido;
+                        this.Descricao = "Devolver após 30 dias vencido";
+                        break;
+                    default:
+                        this.Codigo = 0;
+                        this.Descricao = " (Selecione) ";
+                        break;
 
-				}
+                }
 
-				this.QuantidadeDias = nrDias;
-			}
-			catch (Exception ex)
-			{
-				throw new Exception("Erro ao carregar objeto", ex);
-			}
-		}
+                this.QuantidadeDias = nrDias;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Erro ao carregar objeto", ex);
+            }
+        }
 
 		public override void Valida()
 		{
-			// base.Valida();
+	        base.Valida();
 		}
 
 		#endregion
