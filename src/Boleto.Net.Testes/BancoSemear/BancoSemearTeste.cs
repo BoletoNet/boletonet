9be90b3707b1c98f2f94b0a4e8e86c0470c4d396
Leandro Custodio <leandro@meuacerto.com.br>
2018-01-25 11:10:11 -0200

    Removendo objeto não utilizado durante testes

--- src/Boleto.Net.Testes/BancoSemear/BancoSemearTeste.cs
+++ src/Boleto.Net.Testes/BancoSemear/BancoSemearTeste.cs
@@ -10,57 +10,6 @@ namespace BoletoNet.Testes.BancoSemear
         Boleto boleto = new Boleto();
         Banco banco = new Banco(743);
 
-        public BancoSemearTeste()
-        {
-            boleto.Cedente = new Cedente()
-            {
-                Codigo = "743",
-                MostrarCNPJnoBoleto = true,
-                Nome = "BANCO SEMEAR",
-                CPFCNPJ = "00795423000145",
-                Carteira = "2",
-                DigCedente = "9",
-                ContaBancaria = new ContaBancaria()
-                {
-                    Agencia = "001",
-                    DigitoAgencia = "0",
-                    Conta = "1099681",
-                    DigitoConta = "8"
-                },
-                Endereco = new Endereco()
-            };
-
-            boleto.LocalPagamento = "Este boleto poderá ser pago em toda a Rede Bancária até 5 dias após o vencimento. ";
-            boleto.Instrucoes.Add(new Instrucao_Bradesco()
-            {
-                Descricao = "Não receber após o vencimento",
-                QuantidadeDias = 3
-            });
-
-            boleto.ValorBoleto = 251.51M;
-            boleto.ValorCobrado = 251.51M;
-            boleto.NossoNumero = "35148373401";
-            boleto.NumeroDocumento = "051483734";
-            boleto.DataVencimento = new DateTime(2017, 12, 4);
-            boleto.DataProcessamento = DateTime.Now;
-            boleto.Carteira = "02";
-
-            boleto.Sacado = new Sacado()
-            {
-                CPFCNPJ = "00023128321",
-                Nome = "REGINALDO MENDES DE OLIVEIRA",
-                Endereco = new Endereco()
-                {
-                    Complemento = "MODESTO DE MELO",
-                    Numero = "50",
-                    Bairro = "",
-                    CEP = "73801010",
-                    Cidade = "CENTRO-FORMOSA",
-                    UF = "Goiás",
-                }
-            };
-        }
-
         [TestMethod]
         public void TesteLinhaDigitavel()
         {
