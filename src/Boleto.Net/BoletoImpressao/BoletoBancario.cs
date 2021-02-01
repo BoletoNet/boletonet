4ffc9dc73ab95a165f7ae89f2335f64b0f2ebe8c
Bruno Guelere <brunoguelere@outlook.com.br>
2021-02-01 14:29:32 -0300

    Erros itau
    Erro ao gerar remessa com avalista null e na impressão do boleto corrigido a "Agencia/Cod. Beneficário" para os padroes do manual de integracao que é a Agencia e a conta com o dv.

--- src/Boleto.Net/BoletoImpressao/BoletoBancario.cs
+++ src/Boleto.Net/BoletoImpressao/BoletoBancario.cs
@@ -773,6 +773,10 @@ namespace BoletoNet
                         agenciaCodigoCedente = string.Format("{0}-{1}/{2}-{3}", Cedente.ContaBancaria.Agencia, Cedente.ContaBancaria.DigitoAgencia, Utils.FormatCode(Cedente.ContaBancaria.Conta, 7), Cedente.ContaBancaria.DigitoConta);
                         break;
 
+                    case (int)Bancos.Itau:
+                        agenciaCodigoCedente = $"{Cedente.ContaBancaria.Agencia}/{Utils.FormatCode(Cedente.ContaBancaria.Conta, 7)}-{Cedente.ContaBancaria.DigitoConta}";
+                        break;
+
                     default:
                         agenciaCodigoCedente = string.Format("{0}/{1}-{2}", Cedente.ContaBancaria.Agencia, Utils.FormatCode(Cedente.Codigo, 6), Cedente.DigitoCedente);
                         break;
