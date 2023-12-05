52fa0b24522fab31a1067f5573686d43beb1934a
Emerson Gama <emerson.gama@comprocard.com.br>
2023-12-05 16:42:32 -0300

    Fix: Formatação de campo incompatível com a versão do builder.

--- src/Boleto.Net/BoletoImpressao/BoletoBancario.cs
+++ src/Boleto.Net/BoletoImpressao/BoletoBancario.cs
@@ -523,11 +523,11 @@ namespace BoletoNet
                     return html.ToString().Replace("@INSTRUCOES", _instrucoesHtml).Replace("@QRCODE", "").Replace("@COPIARCOLAR", "");
                 else
                 {
-                    string copiarColar = $"Escolha a forma mais conveniente para realizar seu pagamento: <b><i>Código de Barras</i></b> ou <b><i>QR Code</i></b>. Basta acessar o aplicativo da sua instituição financeira e utilizar apenas uma das opções.<br><br><b>Pix Copia e Cola</b><br><font color='Blue'>{this.Boleto.QRCode}</font>";
+                    string copiarColar = string.Format("Escolha a forma mais conveniente para realizar seu pagamento: <b><i>Código de Barras</i></b> ou <b><i>QR Code</i></b>. Basta acessar o aplicativo da sua instituição financeira e utilizar apenas uma das opções.<br><br><b>Pix Copia e Cola</b><br><font color='Blue'>{0}</font>", this.Boleto.QRCode);
                     string qrCode = QRCodeHelper.GerarQrCode(this.Boleto.QRCode);
                     string fnQRCode = string.Format("data:image/jpeg;base64,{0}", qrCode);
                     return html.ToString().Replace("@INSTRUCOES", _instrucoesHtml)
-                        .Replace("@QRCODE", $"<img class=\"qrcode\" src=\"{fnQRCode}\" />")
+                        .Replace("@QRCODE", string.Format("<img class=\"qrcode\" src=\"{0}\" />", fnQRCode))
                         .Replace("@COPIARCOLAR", copiarColar);
                 }
             }
